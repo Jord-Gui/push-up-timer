473355700e2489990613cf611b9b92ade257b305
Jord <jgui0003@student.monash.edu>
2020-05-21 20:35:33 +1000

    added functionality for hours

--- app.js
+++ app.js
@@ -13,19 +13,22 @@ function toggleStartPauseButton() {
 const startElement = document.getElementById("start");
 const pauseElement = document.getElementById("pause");
 const resetElement = document.getElementById("reset");
-let secondsElement = document.getElementById("seconds");
+let secondElement = document.getElementById("seconds");
 let minuteElement = document.getElementById("minutes");
-const defaultSeconds = secondsElement.textContent;
+let hourElement = document.getElementById("hours");
+const defaultSeconds = secondElement.textContent;
 const defaultMinutes = minuteElement.textContent;
+const defaultHours = hourElement.textContent;
 const timer = document.getElementById("timer");
 let timerInterval = null;
 
 startElement.addEventListener("click", () => {
     if (timerInterval === null) { // check start button hasn't already been pressed
         timerInterval = setInterval(() => {
-            let seconds = parseInt(secondsElement.textContent);
+            let seconds = parseInt(secondElement.textContent);
             let minutes = parseInt(minuteElement.textContent);
-            if (seconds === 0 && minutes === 0) {
+            let hours = parseInt(hourElement.textContent);
+            if (hours === 0 && seconds === 0 && minutes === 0) {
                 timer.style.color = "red";
                 clearInterval(timerInterval);
                 timerInterval = null;
@@ -38,12 +41,22 @@ startElement.addEventListener("click", () => {
                 let newSeconds = seconds - 1;
                 if (newSeconds < 0) {
                     newSeconds = 59;
-                    minutes = minutes - 1;
+                    minutes -= 1;
                 }
+                // debugger
+                let newMinutes = minutes
+                if (newMinutes < 0) {
+                    newMinutes = 59
+                    hours -= 1;
+                }
+                // debugger
                 newSeconds = addZero(newSeconds);
-                newMinutes = addZero(minutes);
-                secondsElement.textContent = newSeconds;
+                newMinutes = addZero(newMinutes);
+                // debugger
+                let newHours = addZero(hours);
+                secondElement.textContent = newSeconds;
                 minuteElement.textContent = newMinutes;
+                hourElement.textContent = newHours;
                 // debugger;
             }
         }, 1000);
@@ -60,8 +73,9 @@ pauseElement.addEventListener("click", () => {
 resetElement.addEventListener("click", () => {
     clearInterval(timerInterval);
     timerInterval = null;
-    secondsElement.textContent = defaultSeconds;
+    secondElement.textContent = defaultSeconds;
     minuteElement.textContent = defaultMinutes;
+    hourElement.textContent = defaultHours;
     startElement.classList.remove("hide-btn");
     pauseElement.classList.add("hide-btn");
     timer.style.color = "black";
